b2dddaffd187f20c830ce20c197cc5fa9ba1654c
YM202c2100 <y.m202c2100@gmail.com>
2024-05-13 17:52:49 +0900

    [add] 一番下まで行くと新たにfetchを行う関数を実行できるよう変更

--- app/components/loadPokemon.tsx
+++ app/components/loadPokemon.tsx
@@ -1,11 +1,41 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { fetchFromPokeAPI } from "../actions/getPokemon"
+import { useInView } from "react-intersection-observer"
+
 interface Props{
   search:string | undefined
 }
 
-const LoadPokemon:React.FC<Props> = ()=>{
-  return(
-    <div>LoadPokemonComponent</div>
-  )
+interface Pokemon{
+  name:string,
+  url:string
+}
+
+const LoadPokemon:React.FC<Props> = ({search})=>{
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  const [page, setPage] = useState(1)
+  const {ref, inView} = useInView()
+
+  const loadMorePokemons = async (page:number)=>{
+    const fetchedPokemons =  await fetchFromPokeAPI({offset:(page-1)*24})
+    setPokemons([...pokemons, ...fetchedPokemons])
+  }
+
+  useEffect(()=>{
+    if(inView){
+      console.log("inview")
+      loadMorePokemons(page)
+    }
+  },[inView])
+
+  return(<>
+    <div className=" bg-slate-500">
+      {pokemons.map(pokemon => <div>{pokemon.name}</div>)}
+    </div>
+    <div ref={ref} className="bg-red-400">inview</div>
+  </>)
 }
 
 export default LoadPokemon
