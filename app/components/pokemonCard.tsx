3b2e3df1f9eb8831ef797d9efc26d0b333e73cbe
YM202c2100 <y.m202c2100@gmail.com>
2024-05-14 00:53:54 +0900

    [add] propsから受け取ったurlからデータをfetch

--- app/components/pokemonCard.tsx
+++ app/components/pokemonCard.tsx
@@ -1,9 +1,31 @@
 import type { Pokemon } from "./loadPokemon"
+import { useEffect, useState } from "react"
 
 interface Props{
   pokemon:Pokemon
 }
+
+interface PokemonInfo{
+  name:string,
+  order:number,
+  types:string[]
+}
 const PokemonCard:React.FC<Props> = ({pokemon}) =>{
+  const [pokemonInfo, setPokemonInfo] = useState<PokemonInfo>()
+
+  const fetchPokemonInfo = async (pokemon:Pokemon)=>{
+    const fetchedInfo:{types:{type:{name:string}}[], order:number} = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`).then(res => res.json())
+    const types:string[] = fetchedInfo.types.map(typeInfo => typeInfo.type.name)
+    const newPokemonInfo:PokemonInfo = {name:pokemon.name,
+                                        order:fetchedInfo.order,
+                                        types:types}
+    setPokemonInfo(newPokemonInfo)
+  }
+
+  useEffect(()=>{
+    fetchPokemonInfo(pokemon)
+  },[])
+
   return(
     <div className="h-[30px] mb-10">{pokemon.name}</div>
   )
