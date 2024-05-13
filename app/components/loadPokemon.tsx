9e3859072ec88d968eaf9b4ad182672135ad179d
YM202c2100 <y.m202c2100@gmail.com>
2024-05-13 14:42:51 +0900

    [add] loadPokemonコンポーネントの定義

---
+++ app/components/loadPokemon.tsx
@@ -0,0 +1,7 @@
+const LoadPokemon:React.FC = ()=>{
+  return(
+    <div>LoadPokemonComponent</div>
+  )
+}
+
+export default LoadPokemon
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import LoadPokemon from "./components/loadPokemon";
 
 export default function Page({searchParams}
 :{
   searchParams:{[key:string]:string | string[] | undefined};
 }){
   
-  return <h1>Root Page</h1>
+  return (<>
+    <LoadPokemon/>
+  </>)
 }
