01c4a1862eb3c995391ec9cb81cf23bd4a6fb046
YM202c2100 <y.m202c2100@gmail.com>
2024-05-14 10:00:25 +0900

    [add] fetchしてくるデータのinterfaceを定義
    urlにorderを入れただけでは、画像を返してくれないものがあったため。(No.75)

--- app/components/pokemonCard.tsx
+++ app/components/pokemonCard.tsx
@@ -7,20 +7,30 @@ interface Props{
   pokemon:Pokemon
 }
 
+interface FetchedDataFormat{
+  name:string,
+  order:number,
+  types:{type:{name:string}}[]
+  sprites:{other:{"official-artwork":{front_default:string, front_shiny:string}}}
+}
+
 interface PokemonInfo{
   name:string,
   order:number,
-  types:string[]
+  types:string[],
+  spriteUrl:string
 }
 const PokemonCard:React.FC<Props> = ({pokemon}) =>{
-  const [pokemonInfo, setPokemonInfo] = useState<PokemonInfo>({name:"bulbasaur", order:0, types:["grass", "pison"]})
+  const [pokemonInfo, setPokemonInfo] = useState<PokemonInfo>({name:"bulbasaur", order:0, types:["grass", "pison"], spriteUrl:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"})
 
   const fetchPokemonInfo = async (pokemon:Pokemon)=>{
-    const fetchedInfo:{types:{type:{name:string}}[], order:number} = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`).then(res => res.json())
+    const fetchedInfo:FetchedDataFormat = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+                                                .then(res => res.json())
     const types:string[] = fetchedInfo.types.map(typeInfo => typeInfo.type.name)
     const newPokemonInfo:PokemonInfo = {name:pokemon.name,
                                         order:fetchedInfo.order,
-                                        types:types}
+                                        types:types,
+                                        spriteUrl:fetchedInfo.sprites.other["official-artwork"].front_default}
     setPokemonInfo(newPokemonInfo)
   }
 
@@ -29,7 +39,7 @@ const PokemonCard:React.FC<Props> = ({pokemon}) =>{
   },[])
 
   return(
-    <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonInfo?.order}.png`}
+    <Image src={pokemonInfo.spriteUrl}
            alt={`Image of ${pokemonInfo?.name}`}
            width={300}
            height={300}
