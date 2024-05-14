2afb4c4387e53c19d58ff07dc25ff7d4c2246ecc
YM202c2100 <y.m202c2100@gmail.com>
2024-05-14 23:40:53 +0900

    [update] routerを使ってクエリ変更時にリクエスト送信

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -1,11 +1,23 @@
 "use client"
 
-import { useState } from "react"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
 
 const Search:React.FC = ()=>{
   const [text, setText] = useState("")
+  const router = useRouter()
+
+  const submitQuery = ()=>{
+    if(text === ""){
+      router.push("./")
+    }else{
+      router.push(`./?search=${text}`)
+    }
+  }
+  useEffect(submitQuery,[text])
+
   return(<>
-    <form action="" onSubmit={(e)=>{e.preventDefault();console.log(`clicked and sent value: ${text}`)}}>
+    <form onSubmit={(e)=>{e.preventDefault();console.log(`clicked and sent value: ${text}`)}}>
       <input type="text"
            onChange={(e)=>setText(e.target.value)} 
            value={text}
