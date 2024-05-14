39a8b4e1a0e4c1e124f01c542bc41fa2285223c2
YM202c2100 <y.m202c2100@gmail.com>
2024-05-14 22:44:45 +0900

    [update] Enterボタンでinput要素の値を出力できるように変更

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -5,12 +5,15 @@ import { useState } from "react"
 const Search:React.FC = ()=>{
   const [text, setText] = useState("")
   return(<>
-    <input type="text"
+    <form action="" onSubmit={(e)=>{e.preventDefault();console.log(`clicked and sent value: ${text}`)}}>
+      <input type="text"
            onChange={(e)=>setText(e.target.value)} 
            value={text}
            className="border"
-    />
-    <button onClick={()=>console.log(`clicked and sent value:${text}`)}>検索</button>
+      />
+      <button>検索</button>
+    </form>
+    
     </>)
 }
 
