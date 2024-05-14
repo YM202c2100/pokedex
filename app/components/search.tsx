ca1d2dc27e304ecfc5266ed2c36a541555dfe3c2
YM202c2100 <y.m202c2100@gmail.com>
2024-05-14 23:59:58 +0900

    [remove] buttonを削除
    onSubmitでurlを更新しようとしてもsubmitQueryがtext stateではなくデバウンスされたquery stateをもとに更新しているのでurlを更新することができないため削除

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -6,7 +6,7 @@ import { useDebounce } from "use-debounce"
 
 const Search:React.FC = ()=>{
   const [text, setText] = useState("")
-  const [query] = useDebounce(text, 750)
+  const [query] = useDebounce(text, 3000)
   const router = useRouter()
 
   const submitQuery = ()=>{
@@ -18,17 +18,15 @@ const Search:React.FC = ()=>{
   }
   useEffect(submitQuery,[query])
 
-  return(<>
-    <form onSubmit={(e)=>{e.preventDefault();console.log(`clicked and sent value: ${text}`)}}>
+  return(
+    <form onSubmit={(e)=>{e.preventDefault()}}>
       <input type="text"
            onChange={(e)=>setText(e.target.value)} 
            value={text}
            className="border"
       />
-      <button>検索</button>
     </form>
-    
-    </>)
+  )
 }
 
 export default Search
