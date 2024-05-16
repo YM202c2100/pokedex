08f00a70745b7910cb74f8ed8f200118e47b2f39
YM202c2100 <y.m202c2100@gmail.com>
2024-05-16 22:56:43 +0900

    [update] fetch時のエンドポイントを静的なurlに変更
    エンドポイントに変化がない場合、フェッチしたデータがキャッシュされるため。

--- app/actions/getPokemon.ts
+++ app/actions/getPokemon.ts
@@ -1,5 +1,5 @@
 const fetchFromPokeAPI = async ({offset=1}:{offset?:number})=> {
-  let endPoint = `https://pokeapi.co/api/v2/pokemon?limit=24&offset=${offset}`
+  let endPoint = `https://pokeapi.co/api/v2/pokemon?limit=10000`
   const fetchedData = await fetch(endPoint).then((result)=>result.json())
   return fetchedData.results
 }
