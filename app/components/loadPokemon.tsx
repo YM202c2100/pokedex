d7499fe4fb5798c7412227954b8ac6101fafa3b5
YM202c2100 <y.m202c2100@gmail.com>
2024-05-19 00:35:19 +0900

    [update] デプロイに伴い見栄えが悪いのでスクロール検知部分の背景色・文字を消去

--- app/components/loadPokemon.tsx
+++ app/components/loadPokemon.tsx
@@ -38,7 +38,7 @@ const LoadPokemon:React.FC<Props> = ({search})=>{
     <div className="flex flex-wrap w-[900px] m-auto">
       {pokemons.map(pokemon => <PokemonCard key={pokemon.name} pokemon={pokemon}/>)}
     </div>
-    <div ref={ref} className="bg-red-400">inview</div>
+    <div ref={ref}></div>
   </>)
 }
 
